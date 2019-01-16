7044c33965fdd4fab76a4cce120dcf0f1806f35a
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-01-16 09:56:46 +0700

    feat(component): implement redux in apps

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import React, { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
+import configureStore from './reducers';
 
 import './style.scss';
 
 const ChatTemplate = lazy(() => import('./template/chat/chat.template'));
-const ChatListingTemplate = lazy(() => import('./template/chat-listing/chat-listing.template'));
 const Loading = lazy(() => import('@/component/loading/loading.component'));
 
+const store = configureStore();
+
 render(
-    <div className="template">
-        <Suspense fallback={null}>
-            <ChatTemplate />
-            <ChatListingTemplate />
-            <Loading />
-        </Suspense>
-    </div>,
+    <Provider store={store}>
+        <div className="template">
+            <Suspense fallback={null}>
+                <ChatTemplate />
+                <Loading />
+            </Suspense>
+        </div>
+    </Provider>,
     document.getElementById('app')
 );
--- src/template/chat/chat.template.js
+++ src/template/chat/chat.template.js
@@ -1,4 +1,7 @@
 import React, { lazy, Suspense } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { showNavbarAction } from '@/actions/status.action';
 
 import './style.scss';
 
@@ -7,19 +10,52 @@ const Header = lazy(() => import('@/component/header/header.component'));
 const ChatContainer = lazy(() => import('@/component/chat-container/chat-container.component'));
 const Welcome = lazy(() => import('@/component/welcome/welcome.component'));
 
+const mapStateToProps = (state) => ({
+    show: state.StatusReducers.show_navbar
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    showNavbar: () => dispatch(showNavbarAction())
+});
+
 class ChatTemplate extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.setShowNavbar = this.setShowNavbar.bind(this);
+    }
+
+    setShowNavbar() {
+        const { showNavbar } = this.props;
+        showNavbar();
+    }
+
     render() {
+        const { show } = this.props;
+
         return (
             <div className="ui-chat-template">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Header />
-                    <Welcome />
-                    <ChatContainer />
-                    <Form disabled />
+                <Suspense fallback={null}>
+                    <Header show={show} />
+                    <Welcome show={!show} onShowWelcome={this.setShowNavbar} />
+                    <ChatContainer show={show} />
+                    <Form disabled={!show} />
                 </Suspense>
             </div>
         );
     }
 }
 
-export default ChatTemplate;
+ChatTemplate.propTypes = {
+    show: PropTypes.bool,
+    showNavbar: PropTypes.func
+};
+
+ChatTemplate.defaultProps = {
+    show: false,
+    showNavbar: () => {}
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ChatTemplate);
